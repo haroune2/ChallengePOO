2f0060495b85451e2f6e5ebae7fec068cbabf470
boumedjirek <haroun902009@hotmail.fr>
2024-01-03 20:01:50 +0100

    first commit

--- Classroom.java
+++ Classroom.java
@@ -1,6 +1,14 @@
 
  
 public class Classroom {
+
+    /*
+    La classe Classroom se compile sans erreur et affiche le résultat attendu dans le terminal
+
+    Je m'appelle haroune et je suis aware
+    Je m'appelle Jean-Claude et je suis aware
+    Je m'appelle Henri et je ne suis pas aware
+ */
     public static void main(String[] args) {
         Wilder wilder1 = new Wilder("haroune", true); 
         Wilder wilder2 = new Wilder("Jean-Claude", true); 
